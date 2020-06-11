9db0ce97fe105e9086e7ece5ff02573df3d184b8
Thiago Ferrari <thiago.ferrari@bpfidelidade.com.br>
2020-06-11 15:58:12 -0300

    alterações

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const vm = new Vue ({
+  el:'#app',
+  data:{
+    text:'Lorem Ipsum available but the  suffered alterat ionsome form humour or randomised words ',
+    text2:'Business',
+    text3:'Events'
+  }
+})
