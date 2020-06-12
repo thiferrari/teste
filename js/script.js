af9f7364d9165564357b4586dc49ce8f1396363c
Thiago Ferrari <thiago.ferrari@bpfidelidade.com.br>
2020-06-12 13:56:44 -0300

    mobile

--- js/script.js
+++ js/script.js
@@ -6,3 +6,7 @@ const vm = new Vue ({
     text3:'Events'
   }
 })
+
+Vue.component('box-text', {
+  template: '<div class="container-text"><div class="text">{{text}}</div><div class="icon-social"><ul><li><a href="#"><img src="img/icons/icon-linkedin.png" class="img-responsive"></a></li><li><a href="#"><img src="img/icons/icon-pinterest.png" class="img-responsive"></a></li><li><a href="#"><img src="img/icons/icon-twitter.png" class="img-responsive"></a></li> <li><a href="#"><img src="img/icons/icon-facebook.png" class="img-responsive"></a></li> </ul></div></div>'
+})
